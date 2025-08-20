031c700cc53e9a47491a784fe4f71320992f9496
Selik-WL <143916977+Selik-WL@users.noreply.github.com>
2025-08-20 14:19:37 -0400

    Update server.js
    adding debugging lines

--- placeholder/server/src/server.js
+++ placeholder/server/src/server.js
@@ -5,6 +5,9 @@ const __dirname = import.meta.dirname;
 const PORT = process.env.PORT || 8519;
 const server = express();
 
+console.log('Serving static from: ', path.join(__dirname, '../../client/dist'));
+console.log('Serving index.html file at: ', path.join(__dirname, '../../client/dist/index.html'));
+
 server.use(express.static(path.join(__dirname, '../../client/dist')));
 server.get('*', (req,res) => res.sendFile(path.join(__dirname, '../../client/dist/index.html')));
 
